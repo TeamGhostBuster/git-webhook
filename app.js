4fa7635a8ecb13b10eb33ae9e77e8a3c2f5d6e56
Michael Lin <linzichunzf@hotmail.com>
2017-02-03 00:36:42 -0700

    rewrite with express

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
-var http = require('http')
 var exec = require('exec')
+var express = require('express')
+var app = express()
 
 const PORT = 9988
 const REPO_URL = 'https://github.com/TeamGhostBuster/server.git'
 const REPO_NAME = 'server'
 
-var deployServer = http.createServer(function (request, response) {
-  if (request.url.search(/deploy\/?$/i) > 0) {
-    var commands = [
-      'rm -rf ' + REPO_NAME,
-      'git clone --no-local ' + REPO_URL,
-      'cd ' + REPO_NAME,
-      'python3 setup.py install --user --prefix=',
-      'forever start -c python3 run.py'
-    ].join(' && ')
+var commands = [
+  'rm -rf ' + REPO_NAME,
+  'git clone --no-local ' + REPO_URL,
+  'cd ' + REPO_NAME,
+  'python3 setup.py install --user --prefix=',
+  'forever start -c python3 run.py'
+].join(' && ')
 
-    exec(commands, function (err, out, code) {
-      if (err instanceof Error) {
-        response.writeHead(500)
-        response.end('Server Internal Error.')
-        throw err
-      }
-      process.stderr.write(err)
-      process.stdout.write(out)
-      response.writeHead(200)
-      response.end('Deploy Done.')
-    })
-  } else {
-    response.writeHead(404)
-    response.end('Not Found.')
-  }
+// deployServer.listen(PORT)
+app.get('/deploy', function (req, res) {
+  exec(commands, function (err, out, code) {
+    if (err instanceof Error) {
+      res.status(500).send('Server Internal Error')
+      throw err
+    }
+    process.stderr.write(err)
+    process.stdout.write(out)
+    res.status(200).send('Deploy Done')
+  })
 })
 
-deployServer.listen(PORT)
+app.listen(PORT, function () {
+  console.log('Git-Webhook started.')
+})
