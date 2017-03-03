cdb738cd7359b3f4a1e676860e23c641a8582713
Michael Lin <linzichunzf@hotmail.com>
2017-03-02 19:33:01 -0700

    update

--- app.js
+++ app.js
@@ -5,36 +5,57 @@ var bodyParser = require('body-parser')
 
 const PORT = 9988
 const REPO_URL = 'https://github.com/TeamGhostBuster/restful-api.git'
-const REPO_NAME = 'server'
+const API_REPO_NAME = 'server'
+const CLIENT_REPO_URL = 'https://github.com/TeamGhostBuster/CollaborativeList.git'
+const CLIENT_REPO_NAME = 'CollaborativeList'
 
 var app = express()
 app.use(json())
 app.use(bodyParser.json())
 
-
-app.post('/restful-api', function (req, res) {
+app.post('/restful-api', function(req, res) {
   commit_sha = console.log(req.body.after)
   var commands = [
-    'cd ' + REPO_NAME,
+    'cd ' + API_REPO_NAME,
     'git reset --hard',
     'git pull',
-    'git config core.sparsecheckout',
-    'git checkout -f ' + commit_sha,
     'docker-compose restart -d'
   ].join(' && ')
-
-  exec(commands, function (err, out, code) {
-    if (err instanceof Error) {
-      res.status(500).send('Server Internal Error')
-      throw err
-    }
-    process.stderr.write(err)
-    process.stdout.write(out)
-    res.status(200).send('Deploy Done')
-  })
+  if (req.body.ref === 'refs/heads/master') {
+    exec(commands, function(err, out, code) {
+      if (err instanceof Error) {
+        res.status(500).send('Server Internal Error')
+        throw err
+      }
+      process.stderr.write(err)
+      process.stdout.write(out)
+      res.status(200).send('Deploy Done')
+    })
+  }
 })
 
+app.post('/raspberry', function(req, res) {
+  commit_sha = console.log(req.body.after)
+  var commands = [
+    'cd ' + CLIENT_REPO_NAME,
+    'git reset --hard',
+    'git pull',
+    'chmod +x build.sh',
+    'sh build.sh'
+  ].join(' && ')
+  if (req.body.ref === 'refs/heads/master') {
+    exec(commands, function(err, out, code) {
+      if (err instanceof Error) {
+        res.status(500).send('Server Internal Error')
+        throw err
+      }
+      process.stderr.write(err)
+      process.stdout.write(out)
+      res.status(200).send('Deploy Done')
+    })
+  }
+})
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log('Git-Webhook started.')
 })
